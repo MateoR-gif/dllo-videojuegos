df4d6e290b4e6481afef721f18d42767f2b02bae
Juan Camilo Barrientos Herrera <jcbarrientos477@gmail.com>
2024-10-27 16:50:21 -0500

    Se agregan sonidos para coleccionables al hacer spawn

--- hello_world/fish_2d.gd
+++ hello_world/fish_2d.gd
@@ -1,13 +1,25 @@
 extends Sprite2D
 
+# Referencia al nodo de sonido
+@onready var sound_player = $spawn
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = false
-	set_randomly()
+	
+	# Llama a la función de spawn con un timeout
+	await spawn_with_timeout()
 
+# Función que maneja el spawn con timeout
+func spawn_with_timeout() -> void:
+	# Esperar 2 segundos
+	await get_tree().create_timer(2.0).timeout
+	
+	set_randomly()
 
 func set_randomly():
 	print("A")
+	
 	var viewport_size = get_viewport_rect().size
 	var new_position = Vector2(
 		randf_range(0, viewport_size.x - texture.get_width()),
@@ -17,3 +29,6 @@ func set_randomly():
 	position = new_position
 	# Muestra el sprite
 	visible = true
+
+	# Reproduce el sonido
+	sound_player.play()
